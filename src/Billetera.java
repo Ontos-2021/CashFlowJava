fee9c5c7bbd985e0c36f31e37dde91dcca3df097
_Ontos_ <josemercado.musica@gmail.com>
2021-09-09 19:07:42 -0300

    Agregué un mainloop básico

--- src/Billetera.java
+++ src/Billetera.java
@@ -16,19 +16,26 @@ public class Billetera {
 
     public void vaciarbilletera() {
         this.saldo = 0;
-        System.out.println("Se ha vaciado la billetera de " + this.dueño + ", el nuevo saldo es: " + this.saldo);
+        System.out.println("Se ha vaciado la billetera de " + this.dueño);
+        mostrarsaldo();
     }
 
     public void ingresardinero (double monto) {
 
         this.saldo += monto;
         System.out.println("Se ha ingresado " + monto + " a la billetera de " + this.dueño);
+        mostrarsaldo();
     }
 
     public void retirardinero (double monto) {
 
         this.saldo -= monto;
         System.out.println("Se ha retirado " + monto + " a la billetera de " + this.dueño);
+        mostrarsaldo();
+    }
+
+    public void mostrarsaldo() {
+        System.out.println("El saldo de la billetera de " + this.dueño + " es: " + this.saldo);
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
         JOptionPane.showMessageDialog(null, "¡Hola! ¡Bienvenido al juego Cash Flow!", "Cash Flow", JOptionPane.PLAIN_MESSAGE);
 
+        // Crear jugador
+
         String nombre = JOptionPane.showInputDialog("¿Cuál es su nombre?");
 
         int edad = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos años tienes?"));
@@ -12,5 +14,33 @@ public class Main {
         double saldo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto dinero tienes?"));
 
         Jugador jugador_1 = new Jugador(nombre, edad, saldo);
+
+        // mainloop
+
+        System.out.println("");
+
+        boolean pregunar_otra_vez = true;
+
+        do {
+            int respuesta = Integer.parseInt(JOptionPane.showInputDialog("Qué quieres hacer?\n1- Ingresar Dinero\n2- Retirar Dinero\nIngrese otro número para salir"));
+            if (respuesta == 1) {
+
+                double monto = Double.parseDouble(JOptionPane.showInputDialog("Cuánto quieres ingresar?"));
+                jugador_1.billetera.ingresardinero(monto);
+
+            } else if (respuesta == 2) {
+
+                double monto = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto quieres retirar?"));
+                jugador_1.billetera.retirardinero(monto);
+
+            } else {
+                pregunar_otra_vez = false;
+                JOptionPane.showMessageDialog(null, "Parece que ingresó mal un dato\nEl programa se cerrará");
+            }
+
+            System.out.println("");
+
+        } while (pregunar_otra_vez);
+
     }
 }
