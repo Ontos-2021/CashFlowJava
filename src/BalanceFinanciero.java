b12f6ad72f0d6836a11cb798af894558b5d48ed0
_Ontos_ <josemercado.musica@gmail.com>
2021-09-10 22:54:20 -0300

    Agregué la clase Balance Financiero y modifiqué .gitignore

---
+++ src/BalanceFinanciero.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class BalanceFinanciero {
+    Jugador propietario;
+    double ingresos_pasivos, ingresos_activos, salario, gastos_totales;
+    ArrayList<Activo> activos = new ArrayList<Activo>();
+
+    public BalanceFinanciero(double salario) {
+        this.ingresos_pasivos = 0;
+        this.ingresos_activos = 0;
+        this.salario = salario;
+        this.gastos_totales = 0;
+        this.activos = activos;
+    }
+}
--- src/Jugador.java
+++ src/Jugador.java
@@ -6,14 +6,15 @@ public class Jugador {
     double saldo;
     double salario;
     Billetera billetera;
-    ArrayList <Activo> activos = new ArrayList<Activo>();
     double ingresos_pasivos;
+    BalanceFinanciero balance;
 
     public Jugador(String nombre, int edad, double saldo, double salario) {
         this.nombre = nombre;
         this.edad = edad;
         this.billetera = new Billetera(nombre, saldo);
         this.salario = salario;
+        this.balance = new BalanceFinanciero(salario);
         System.out.println("¡Hola!, soy " + nombre + " y tengo " + edad + " años");
     }
 
@@ -23,11 +24,11 @@ public class Jugador {
         this.billetera = new Billetera(nombre);
     }
     public void comprar_activo(Activo activo) {
-        activos.add(activo);
+        this.balance.activos.add(activo);
         System.out.println("¡Felicitaciones " + this.nombre +"! Se ha agregado el siguiente activo a su portafolio: " + activo.nombre);
         this.billetera.retirardinero(activo.precio);
         ingresos_pasivos = 0;
-        activos.forEach(activo_en_lista -> this.ingresos_pasivos += activo_en_lista.ingreso_pasivo);
+        this.balance.activos.forEach(activo_en_lista -> this.ingresos_pasivos += activo_en_lista.ingreso_pasivo);
         System.out.println("Los ingresos pasivos totales son: " + ingresos_pasivos);
     }
 }
