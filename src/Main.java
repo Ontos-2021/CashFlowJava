4c730d725001105e42389246fb4e79b0e9e8f59b
_Ontos_ <josemercado.musica@gmail.com>
2021-09-20 01:54:35 -0300

    Creé funciones para las opciones del menú

--- src/Main.java
+++ src/Main.java
@@ -29,29 +29,19 @@ public class Main {
             double monto;
             switch (respuesta){
                 case 1:
-                    monto = Double.parseDouble(JOptionPane.showInputDialog("Cuánto quieres ingresar?"));
-                    jugador_1.balance.billetera.ingresardinero(monto);
+                    ingresar_dinero(jugador_1);
                     break;
                 case 2:
-                    monto = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto quieres retirar?"));
-                    jugador_1.balance.billetera.retirardinero(monto);
+                    retirar_dinero(jugador_1);
                     break;
                 case 3:
                     diadepago dia_de_pago = new diadepago(jugador_1);
                     break;
                 case 4:
-                    String nombre_activo = JOptionPane.showInputDialog("¿Qué es lo que quiere comprar?");
-                    double precio_activo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto cuesta?"));
-                    double ingreso_pasivo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto es el ingreso pasivo que genera?"));
-                    Activo activo = new Activo(nombre_activo, precio_activo, ingreso_pasivo);
-                    jugador_1.comprar_activo(activo);
+                    comprar_activo(jugador_1);
                     break;
                 case 5:
-                    String nombre_pasivo = JOptionPane.showInputDialog("¿Qué es lo que quiere comprar?");
-                    double precio_pasivo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto cuesta?"));
-                    double gasto_pasivo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto es el gasto pasivo que genera?"));
-                    Pasivo pasivo = new Pasivo(nombre_pasivo, precio_pasivo, gasto_pasivo);
-                    jugador_1.comprar_pasivo(pasivo);
+                    comprar_pasivo(jugador_1);
                     break;
 
                 default:
@@ -65,5 +55,31 @@ public class Main {
             System.out.println("");
 
         } while (preguntar_otra_vez);
+    }
+
+    public static void ingresar_dinero(Jugador jugador) {
+        double monto = Double.parseDouble(JOptionPane.showInputDialog("Cuánto quieres ingresar?"));
+        jugador.balance.billetera.ingresardinero(monto);
+    }
+
+    public static void retirar_dinero(Jugador jugador) {
+        double monto = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto quieres retirar?"));
+        jugador.balance.billetera.retirardinero(monto);
+    }
+
+    public static void comprar_activo(Jugador jugador) {
+        String nombre_activo = JOptionPane.showInputDialog("¿Qué es lo que quiere comprar?");
+        double precio_activo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto cuesta?"));
+        double ingreso_pasivo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto es el ingreso pasivo que genera?"));
+        Activo activo = new Activo(nombre_activo, precio_activo, ingreso_pasivo);
+        jugador.comprar_activo(activo);
+    }
+
+    public static void comprar_pasivo(Jugador jugador) {
+        String nombre_pasivo = JOptionPane.showInputDialog("¿Qué es lo que quiere comprar?");
+        double precio_pasivo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto cuesta?"));
+        double gasto_pasivo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto es el gasto pasivo que genera?"));
+        Pasivo pasivo = new Pasivo(nombre_pasivo, precio_pasivo, gasto_pasivo);
+        jugador.comprar_pasivo(pasivo);
     }
 }
