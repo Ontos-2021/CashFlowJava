8cb4e796edda20c3e8ce6ae282080feef1f67728
_Ontos_ <josemercado.musica@gmail.com>
2021-09-20 00:42:00 -0300

    Limpié el código de día de pago

--- src/diadepago.java
+++ src/diadepago.java
@@ -16,10 +16,11 @@ public class diadepago {
         JOptionPane.showMessageDialog(null, "¡Felicitaciones " + this.nombre + "! ¡Ha llegado el día de pago!");
         JOptionPane.showMessageDialog(null, "Salario: " + this.salario + "\nIngreso Pasivo: " + this.ingreso_pasivo + "\nGastos pasivos: " + this.gastos_pasivos + "\nGastos fijos: " + this.gastos_fijos + "\nCashflow: " + this.cashflow);
         System.out.println("¡Día de pago!");
-        System.out.println("Has recibido un salario de " + this.salario);
-        System.out.println("Has recibido ingresos pasivos por " + this.ingreso_pasivo);
-        System.out.println("Gastos pasivos: " + jugador.balance.gastos_pasivos);
-        System.out.println("Gastos fijos: " + jugador.balance.gastos_fijos);
+        System.out.println("Salario: " + this.salario);
+        System.out.println("Ingresos pasivos: " + this.ingreso_pasivo);
+        System.out.println("Gastos pasivos: " + this.gastos_pasivos);
+        System.out.println("Gastos fijos: " + this.gastos_fijos);
+        System.out.println("Cashflow: " + this.cashflow);
         jugador.balance.billetera.ingresardinero(this.salario + this.ingreso_pasivo);
         jugador.balance.billetera.retirardinero(this.gastos_pasivos + this.gastos_fijos);
     }
