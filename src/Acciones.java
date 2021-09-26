58bb10e3a2226913135cccd9bf2e34995d8e59e8
_Ontos_ <josemercado.musica@gmail.com>
2021-09-26 13:15:14 -0300

    Limpié las funciones de la interfaz Acciones

--- src/Acciones.java
+++ src/Acciones.java
@@ -3,46 +3,53 @@ import javax.swing.*;
 public interface Acciones {
 
     static void ingresar_dinero(Jugador jugador) {
-        double monto = Double.parseDouble(JOptionPane.showInputDialog("Cuánto quieres ingresar?"));
+        double monto = Mensajes.input_double("¿Cuánto quieres ingresar?");
         jugador.balance.billetera.ingresardinero(monto);
     }
 
     static void retirar_dinero(Jugador jugador) {
-        double monto = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto quieres retirar?"));
+        double monto = Mensajes.input_double("¿Cuánto quieres retirar?");
         jugador.balance.billetera.retirardinero(monto);
     }
 
     static void comprar_activo(Jugador jugador) {
-        String nombre_activo = JOptionPane.showInputDialog("¿Qué es lo que quiere comprar?");
-        double precio_activo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto cuesta?"));
-        double ingreso_pasivo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto es el ingreso pasivo que genera?"));
+        String nombre_activo = Mensajes.input_string("¿Cuál es el activo que quiere comprar?");
+        double precio_activo = Mensajes.input_double("¿Cuánto cuesta?");
+        double ingreso_pasivo = Mensajes.input_double("Cuánto es el ingreso pasivo que genera?");
+
         Activo activo = new Activo(nombre_activo, precio_activo, ingreso_pasivo);
         jugador.comprar_activo(activo);
+
     }
 
     static void comprar_pasivo(Jugador jugador) {
-        String nombre_pasivo = JOptionPane.showInputDialog("¿Qué es lo que quiere comprar?");
-        double precio_pasivo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto cuesta?"));
-        double gasto_pasivo = Double.parseDouble(JOptionPane.showInputDialog("¿Cuánto es el gasto pasivo que genera?"));
+        String nombre_pasivo = Mensajes.input_string("¿Cuál es el pasivo que quiere comprar?");
+        double precio_pasivo = Mensajes.input_double("¿Cuánto cuésta?");
+        double gasto_pasivo = Mensajes.input_double("¿Cuánto es el gasto pasivo que genera?");
+
         Pasivo pasivo = new Pasivo(nombre_pasivo, precio_pasivo, gasto_pasivo);
+
         jugador.comprar_pasivo(pasivo);
     }
 
     static void dia_de_pago(Jugador jugador) {
         String mensaje = "¡Felicitaciones " + jugador.nombre + "! ¡Ha llegado el día de pago!";
         Mensajes.show(mensaje, "Día de Pago");
+
         mensaje = "Salario: " + jugador.balance.salario;
         mensaje += "\nIngreso Pasivo: " + jugador.balance.ingresos_pasivos;
         mensaje += "\nGastos pasivos: " + jugador.balance.gastos_pasivos;
         mensaje += "\nGastos fijos: " + jugador.balance.gastos_fijos;
         mensaje += "\nCashflow: " + jugador.balance.cashflow;
         Mensajes.show(mensaje, "Balance mensual");
+
         System.out.println("¡Día de pago!");
         System.out.println("Salario: " + jugador.balance.salario);
         System.out.println("Ingresos pasivos: " + jugador.balance.ingresos_pasivos);
         System.out.println("Gastos pasivos: " + jugador.balance.gastos_pasivos);
         System.out.println("Gastos fijos: " + jugador.balance.gastos_fijos);
         System.out.println("Cashflow: " + jugador.balance.cashflow);
+
         jugador.balance.billetera.ingresardinero(jugador.balance.salario + jugador.balance.ingresos_pasivos);
         jugador.balance.billetera.retirardinero(jugador.balance.gastos_pasivos + jugador.balance.gastos_fijos);
     }
@@ -52,7 +59,9 @@ public interface Acciones {
         int edad = Mensajes.input_int("¿Cuántos años tienes");
         double salario = Mensajes.input_double("¿Cuál es tu salario?");
         double saldo = Mensajes.input_double("¿Cuanto dinero tienes en tu billetera?");
+
         Jugador jugador = new Jugador(nombre, edad, saldo, salario);
+
         return jugador;
     }
 }
